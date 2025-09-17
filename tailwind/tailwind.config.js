7b5a42cf22533bfc0da223fe560153703d63b6f1
Kalyanimuthu <kalyaniram@gmail.com>
2025-09-18 02:18:14 +0530

    Add bootstrap, css, html, tailwind folders

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // <--- Enable toggling with .dark class
+  content: ["./**/*.html"], // adjust according to your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
